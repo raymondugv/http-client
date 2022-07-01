27f670252a89e1bb5adc643a13bbfa4568baf154
Cam <github@lekienhoanh.dev>
2022-07-01 16:47:30 +0700

    update main function

--- index.php
+++ index.php
@@ -3,34 +3,55 @@
 namespace Httpful;
 
 require_once 'vendor/autoload.php';
-use Httpful\Httpclient\Utils;
-use Httpful\RestAPI;
+use Httpful\Httpclient\APIResponse;
+use Httpful\RequestAPI;
 
 function getRequest()
 {
 
-    $response = RestAPI::get('https://jsonplaceholder.typicode.com/');
-    Utils::dumpResponse($response);
+    $response = RequestAPI::get('https://jsonplaceholder.typicode.com/');
+    APIResponse::dumpResponse($response);
 }
 
 function postRequest()
 {
 
-    $response = RestAPI::post('https://postman-echo.com/post', 'Any Thing');
-    Utils::dumpResponse($response);
+    $response = RequestAPI::post('https://postman-echo.com/post', 'Any Thing');
+    APIResponse::dumpResponse($response);
 
 }
 
 function postRequestWithJson()
 {
-    $response = RestAPI::post(
+    $response = RequestAPI::post(
         'https://postman-echo.com/post',
         ['foo' => 'bar', 'lorem' => 'ipsum'],
         ['content-type' => 'application/json']
     );
-    Utils::dumpResponse($response);
+    APIResponse::dumpResponse($response);
+}
+
+function sendAssessment()
+{
+    $tokenResponse = RequestAPI::options('https://corednacom/corewebdna.com/assessment-endpoint.php');
+
+    $response = RequestAPI::post(
+        'https://corednacom.corewebdna.com/assessment-endpoint.php',
+        [
+            'name' => 'Hoanh Le Kien',
+            'email' => 'lkhoanh@cmcglobal.vn',
+            'url' => 'https://github.com/raymondugv/http-client',
+        ],
+        [
+            'Authorization' => 'Bearer ' . $tokenResponse->getBody(),
+            'content-type' => 'application/json',
+        ]
+    );
+
+    APIResponse::dumpResponse($response);
 }
 
 //getRequest();
 //postRequest();
 //postRequestWithJson();
+// sendAssessment();
